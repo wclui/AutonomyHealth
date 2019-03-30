ae8fe62be40fbac90e8fa9ef7c1a171035fa66e9
Melody Lui <melodylui@melodys-macbook-pro.local>
2019-03-30 10:17:13 -0400

    Initial Commit

---
+++ MedSafe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MedSafe
+//
+//  Created by Melody Lui on 2019-03-30.
+//  Copyright © 2019 Melody Lui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
