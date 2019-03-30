3e9310e701df4807e63bfef8dbf1c7b3e72d077a
Melody Lui <melodylui@Melodys-MacBook-Pro.local>
2019-03-30 18:54:21 -0400

    first commit

--- MedSafe/ViewController.swift
+++ MedSafe/ViewController.swift
@@ -7,14 +7,168 @@
 //
 
 import UIKit
+import Speech
 
 class ViewController: UIViewController {
 
+ 
+    @IBOutlet weak var startStopBtn: UIButton!
+    
+    @IBOutlet weak var segmentCtrl: UISegmentedControl!
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    private var speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "en-US")) //1
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private var audioEngine = AVAudioEngine()
+    var lang: String = "en-US"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        startStopBtn.isEnabled = false  //2
+        speechRecognizer?.delegate = self as? SFSpeechRecognizerDelegate  //3
+        speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: lang))
+        SFSpeechRecognizer.requestAuthorization { (authStatus) in  //4
+            
+            var isButtonEnabled = false
+            
+            switch authStatus {  //5
+            case .authorized:
+                isButtonEnabled = true
+                
+            case .denied:
+                isButtonEnabled = false
+                print("User denied access to speech recognition")
+                
+            case .restricted:
+                isButtonEnabled = false
+                print("Speech recognition restricted on this device")
+                
+            case .notDetermined:
+                isButtonEnabled = false
+                print("Speech recognition not yet authorized")
+            }
+            
+            OperationQueue.main.addOperation() {
+                self.startStopBtn.isEnabled = isButtonEnabled
+            }
+        }
     }
+    @IBAction func segmentAct(_ sender: Any) {
+        switch segmentCtrl.selectedSegmentIndex {
+        case 0:
+            lang = "en-US"
+            break;
+        case 1:
+            lang = "fr-FR"
+            break;
+        case 2:
+            lang = "es"
+            break;
+        case 3:
+            lang = "zh-Hans"
+            break;
+        case 4:
+            lang = "ru"
+            break;
+        case 5:
+            lang = "hi"
+            break;
+        default:
+            lang = "en-US"
+            break;
+        }
+        
+        
+        speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: lang))
+    }
+    
+    @IBAction func startStopAct(_ sender: Any) {
+        speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: lang))
+        
+        if audioEngine.isRunning {
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+            startStopBtn.isEnabled = false
+            startStopBtn.setTitle("Start Recording", for: .normal)
+        } else {
+            startRecording()
+            startStopBtn.setTitle("Stop Recording", for: .normal)
+        }
+    }
+    func startRecording() {
+        
+        if recognitionTask != nil {
+            recognitionTask?.cancel()
+            recognitionTask = nil
+        }
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            //try audioSession.setCategory(AVAudioSession.Category.record)
+            try audioSession.setMode(AVAudioSession.Mode.measurement)
+            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
 
-
+        } catch {
+            print("audioSession properties weren't set because of an error.")
+        }
+        
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        let inputNode = audioEngine.inputNode
+        
+        guard let recognitionRequest = recognitionRequest else {
+            fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
+        }
+        
+        recognitionRequest.shouldReportPartialResults = true
+        
+        recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
+            
+            var isFinal = false
+            
+            if result != nil {
+                
+                self.textView.text = result?.bestTranscription.formattedString
+                isFinal = (result?.isFinal)!
+            }
+            
+            if error != nil || isFinal {
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                
+                self.startStopBtn.isEnabled = true
+            }
+        })
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
+            self.recognitionRequest?.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        
+        do {
+            try audioEngine.start()
+        } catch {
+            print("audioEngine couldn't start because of an error.")
+        }
+        
+        textView.text = "Say something, I'm listening!"
+        
+    }
+    
+    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            startStopBtn.isEnabled = true
+        } else {
+            startStopBtn.isEnabled = false
+        }
+    }
 }
 
