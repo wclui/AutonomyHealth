57f45f0f9dbf86b9808e7ad478a75afdcda050c9
Prasann <prasann6897@gmail.com>
2019-03-31 05:40:58 -0400

    ML model added

--- MedSafe/TestMLStuff.swift
+++ MedSafe/TestMLStuff.swift
@@ -11,15 +11,24 @@ import NaturalLanguage
 
 public struct TestML{
     public static func summarize(text: String, completion: @escaping (String?, Error?) -> Void) {
+        var sen_list = [String]()
 //        print(text);
 //         Split text into sentences
         let tokenizer = NLTokenizer(unit: .sentence)
         tokenizer.string = text
 
         tokenizer.enumerateTokens(in: text.startIndex..<text.endIndex) { tokenRange, _ in
-//            print(text[tokenRange])
+            sen_list.append(String(text[tokenRange]))
             return true
         }
+        do {
+            let textPredictor = try NLModel(mlModel: textClassifier().model)
+            print(textPredictor.predictedLabel(for: "get a blood test done"))
+
+        } catch let error as NSError {
+            print(error)
+        }
+       
         
     }
 }
